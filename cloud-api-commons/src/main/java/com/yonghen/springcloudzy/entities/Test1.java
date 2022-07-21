4ddffbc790cde9a01243bcc2657e10749f2bcd08
YongHen <779600459@qq.com>
2022-07-21 23:06:08 +0800

    2022/07/21---23:05 版本1.1

---
+++ cloud-api-commons/src/main/java/com/yonghen/springcloudzy/entities/Test1.java
@@ -0,0 +1,14 @@
+package com.yonghen.springcloudzy.entities;
+
+/**
+ * @Author: ZhangYi
+ * @Date: 2022/07/21/  23:03
+ * @Description:
+ */
+public class Test1 {
+
+    public void test1()
+    {
+        int a = 10;
+    }
+}
